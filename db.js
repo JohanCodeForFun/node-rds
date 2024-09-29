d90d692555ca242c8b8dc336749e2112ca473ec5
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-09-29 21:55:28 +0200

    fix typo

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const { Client } = pg;
 // Database connection configuration
 const dbConfig = {
   user: process.env.DB_USER,
-  password: process.env.PASSWORD,
+  password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
