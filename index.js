9c52c2fa37f7a8dd2e64de19bb326610b68c7ac5
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-09-29 21:56:24 +0200

    add get from db

--- index.js
+++ index.js
@@ -1,39 +1,46 @@
 const express = require("express");
 const client = require("./db");
+const seedDatabase = require("./dbseed");
 const app = express();
 
 app.get("/", (req, res) => res.send("Hello, world!"));
+app.get("/seed", async (req, res) => {
+  try {
+    await seedDatabase();
+    res.send("Database tables created successfully.");
+  } catch (error) {
+    res.status(500).send("Error creating database tables.");
+  }
+});
+
 app.get("/ping", (req, res) => res.send("pong"));
 
 const PORT = 3000;
-app.listen(PORT, () => console.log(`My first Express app - listening on port ${PORT}!`));
+app.listen(PORT, () => console.log(`Express server - listening on port ${PORT}!`));
 
+client
+	.connect()
+	.then(() => {
+		console.log('Connected to PostgreSQL database');
 
-// client
-// 	.connect()
-// 	.then(() => {
-// 		console.log('Connected to PostgreSQL database');
+		client.query('SELECT * FROM reviews', (err, result) => {
+			if (err) {
+				console.error('Error executing query', err);
+			} else {
+				console.log('Query result:', result.rows);
+			}
 
-// 		// Execute SQL queries here
-
-// 		client.query('SELECT * FROM reviews', (err, result) => {
-// 			if (err) {
-// 				console.error('Error executing query', err);
-// 			} else {
-// 				console.log('Query result:', result.rows);
-// 			}
-
-// 			// Close the connection when done
-// 			client
-// 				.end()
-// 				.then(() => {
-// 					console.log('Connection to PostgreSQL closed');
-// 				})
-// 				.catch((err) => {
-// 					console.error('Error closing connection', err);
-// 				});
-// 		});
-// 	})
-// 	.catch((err) => {
-// 		console.error('Error connecting to PostgreSQL database', err);
-// 	});
+			// Close the connection when done
+			client
+				.end()
+				.then(() => {
+					console.log('Connection to PostgreSQL closed');
+				})
+				.catch((err) => {
+					console.error('Error closing connection', err);
+				});
+		});
+	})
+	.catch((err) => {
+		console.error('Error connecting to PostgreSQL database', err);
+	});
